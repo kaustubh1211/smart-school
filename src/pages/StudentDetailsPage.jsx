4ee8209b11a5cb711e222b32c671b7401f8c016f
yashrajvrma <vermayashraj2005@gmail.com>
2025-01-15 09:24:12 +0530

    change src name

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MasterLayout from "../masterLayout/MasterLayout";
-import Breadcrumb from "../../src/components/Breadcrumb";
+import Breadcrumb from "../components/Breadcrumb";
 import StudentDetailsLayer from "../components/StudentDetailsLayer";
 
 const StudentDetailsPage = () => {
