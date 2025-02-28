34fb7d0e33646fb1339363eee8adf6af77d04476
Sahil <shirsekarsahil99@gmail.com>
2025-02-28 11:56:19 +0530

    parent details updated

--- src/components/StudentAdmissionLayer.jsx
+++ src/components/StudentAdmissionLayer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import StudentAdmissionForm from "./child/StudentAdmissionForm";
-import StudentRegistrationForm from "./child/StudentRegistrationForm";
 // import InputFormWithIcons from "./child/InputFormWithIcons";
 // import HorizontalInputForm from "./child/HorizontalInputForm";
 // import HorizontalInputFormWithIcons from "./child/HorizontalInputFormWithIcons";
@@ -10,7 +9,6 @@ const StudentAdmissionLayer = () => {
     <div className="row gy-4">
       {/* VerticalInputForm */}
       <StudentAdmissionForm />
-      {/* <StudentRegistrationForm /> */}
 
       {/* InputFormWithIcons */}
       {/* <InputFormWithIcons /> */}
